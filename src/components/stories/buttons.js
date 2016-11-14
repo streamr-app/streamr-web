680af733cd9d9f0c9fd1ff54dbb378955fd6ea3b
Steven Petryk <me@stevenpetryk.com>
2016-11-14 16:09:28 -0500

    Add action to button story

--- src/components/stories/buttons.js
+++ src/components/stories/buttons.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/storybook'
 
 import { Button } from '../buttons'
 
 storiesOf('Buttons', module)
   .add('Standard button', () => (
-    <Button>I'm a button!</Button>
+    <Button onClick={action('Clicked')}>I'm a button!</Button>
   ))
