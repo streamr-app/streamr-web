c653782fc56d3146009360d41bd793c2b7c6382e
Steven Petryk <me@stevenpetryk.com>
2016-11-25 14:07:46 -0500

    Fix APIAction type

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,11 @@
 // @flow
 
-import { CALL_API } from 'redux-api-middleware'
-
 export type Action =
     { type: 'FIRST' }
   | { type: 'SECOND' }
 
-export type Dispatch = (action: Action | ThunkAction | PromiseAction | APIAction | Array<Action>) => any;
-export type GetState = () => Object;
-export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
-export type PromiseAction = Promise<Action>;
-export type APIAction = { [CALL_API]: Object }
+export type Dispatch = (action: Action | ThunkAction | PromiseAction | APIAction | Array<Action>) => any
+export type GetState = () => Object
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any
+export type PromiseAction = Promise<Action>
+export type APIAction = Object
