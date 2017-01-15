5fd6daa141e4a9d61e186018043160b5af51965b
Tyler Petresky <tnpetresky@gmail.com>
2017-01-15 17:23:11 -0500

    Fixed tests

--- src/components/stories/channel.js
+++ src/components/stories/channel.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
+import { storiesOf } from '@kadira/storybook'
 
 import channel from './data/channel'
 
 import { ChannelBadge } from '../channel/ChannelBadge'
 import { SubscribeButton } from '../channel/SubscribeButton'
-// import { StreamList } from '../streams/StreamList'
 
 storiesOf('Channel', module)
   .add('ChannelBadge', () => (
-    <div style={ {width: '335px'} }>
+    <div style={{width: '335px'}}>
       <ChannelBadge channel={channel} />
     </div>
   ))
   .add('SubscribeButton', () => (
-    <SubscribeButton channel={channel}/>
+    <SubscribeButton channel={channel} />
   ))
   .add('SubscribeButton with Sub Count', () => (
     <SubscribeButton channel={channel} showSubscribers />
