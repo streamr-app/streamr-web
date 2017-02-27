3db1550b6bf6c5fcc7ebd0f956d411a968a714f9
Steven Petryk <me@stevenpetryk.com>
2017-02-26 22:14:57 -0500

    Un-hardcode things

--- src/actions/lines.js
+++ src/actions/lines.js
@@ -1,11 +1,11 @@
 import { createResource } from './index'
 
-export function persistCurrentLine (streamId = 27) {
+export function persistCurrentLine (streamId) {
   return (dispatch, getState) => {
     const line = getState().drawing.currentLine
 
     dispatch(createResource({
-      url: `/streams/27/add_line`,
+      url: `/streams/${streamId}/add_line`,
       types: ['PERSIST_CURRENT_LINE_REQUEST', 'PERSIST_CURRENT_LINE_SUCCESS', 'PERSIST_CURRENT_LINE_FAILURE'],
       authenticated: true,
       body: {
