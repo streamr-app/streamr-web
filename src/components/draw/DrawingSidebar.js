c31ebd38a6f8ab2046339fa57c92423d552c64d7
Steven Petryk <me@stevenpetryk.com>
2017-03-15 20:11:14 -0400

    Name things more consistently

--- src/components/draw/DrawingSidebar.js
+++ src/components/draw/DrawingSidebar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import ColorPickerContainer from '../../containers/draw/ColorPickerContainer'
-import StrokeWidthPickerContainer from '../../containers/draw/StrokeWidthPickerContainer'
+import BrushThicknessPickerContainer from '../../containers/draw/BrushThicknessPickerContainer'
 
 export default () => (
   <div className='drawing-sidebar'>
     <ColorPickerContainer />
-    <StrokeWidthPickerContainer />
+    <BrushThicknessPickerContainer />
   </div>
 )
