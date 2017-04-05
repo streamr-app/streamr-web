6e88eeb8bbe7a16655bef52426091c26cf39198b
Steven Petryk <me@stevenpetryk.com>
2017-04-05 11:34:10 -0400

    Improve profile screen

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import ChannelBadge from './channel/ChannelBadge'
+import ProfileImage from './users/ProfileImage'
+
+import { Banner } from './common/banners'
 import SubscribeButtonContainer from './users/SubscribeButtonContainer'
 import StreamList from './streams/StreamList'
 import StreamCard from './streams/StreamCard'
@@ -15,29 +17,21 @@ export default ({
   user = {},
   streams = []
 }) => (
-  <div className='channel container'>
-    { user ? (
-      <div>
-        <div className='top-wrapper'>
-          <span className='left'>
-            <ChannelBadge channel={user} hideSubscribe />
-          </span>
-          <span className='right'>
-            <SubscribeButtonContainer userId={user.id} />
-          </span>
-        </div>
-        <div className='bottom-wrapper'>
-          <StreamList>
-            {
-              streams.length > 0 ? listStreamCards(streams, user) : (
-                <div className='no-streams'>This channel has not posted any streams.</div>
-              )
-            }
-          </StreamList>
-        </div>
-      </div>
-    ) : (
-      <i className='loading fa fa-spinner fa-spin' />
-    ) }
+  <div className='channel'>
+    <Banner className='channel-overview'>
+      <ProfileImage image={user.image} />
+      <h2>{user.name}</h2>
+      <SubscribeButtonContainer userId={user.id} />
+    </Banner>
+
+    <div className='container'>
+      <StreamList>
+        {
+          streams.length > 0 ? listStreamCards(streams, user) : (
+            <div className='no-streams'>This channel has not posted any streams.</div>
+          )
+        }
+      </StreamList>
+    </div>
   </div>
 )
