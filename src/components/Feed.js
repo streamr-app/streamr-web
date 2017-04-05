d3e5c5a40dd8c0d9309f3253323ef01e8937e8bf
Steven Petryk <me@stevenpetryk.com>
2017-04-04 23:29:13 -0400

    Remove trending topics from home page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
 import SubscriptionStreamsContainer from '../containers/streams/SubscriptionStreamsContainer'
-import TrendingTopicsContainer from '../containers/topics/TrendingTopicsContainer'
 import TrendingStreamsContainer from '../containers/streams/TrendingStreamsContainer'
 
 export default () => (
   <div className='feed container'>
     <h2>My Subscriptions</h2>
     <SubscriptionStreamsContainer />
-
-    <h2>Featured Topics</h2>
-    <TrendingTopicsContainer />
 
     <h2>🚀 Trending Streams</h2>
     <TrendingStreamsContainer />
