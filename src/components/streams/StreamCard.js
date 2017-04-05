1be27a610e7c7602057da49c5c3f12085f76a334
Steven Petryk <me@stevenpetryk.com>
2017-04-04 22:54:39 -0400

    Improve StreamCard appearance

--- src/components/streams/StreamCard.js
+++ src/components/streams/StreamCard.js
@@ -10,11 +10,25 @@ export default ({
   <Link to={`/${stream.slug}`} className={`stream-card ${className}`}>
     <div className='thumbnail-wrapper'>
       <img className='thumbnail' src={stream.image} />
+
+      <p className='duration'>{formatDuration(stream.duration)}</p>
     </div>
+
     <div className='information-wrapper'>
-      <div className='title'>{stream.title}</div>
-      <div className='author'>{user.name}</div>
-      <div className='description'>{stream.description}</div>
+      <p className='title'>{stream.title}</p>
+      <p className='author'>{user.name}</p>
     </div>
   </Link>
 )
+
+function formatDuration (duration = 0) {
+  const minutes = Math.floor(duration / 60)
+  const seconds = duration % 60
+
+  return `${padTime(minutes)}:${padTime(seconds)}`
+}
+
+function padTime (time) {
+  time = time.toString()
+  return ('00' + time).substring(time.length)
+}
