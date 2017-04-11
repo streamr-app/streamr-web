998e24cb7300babd10e367770e3ddf3732282fba
Steven Petryk <me@stevenpetryk.com>
2017-04-10 22:03:38 -0400

    Remove react-recorder-redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { routerReducer } from 'react-router-redux'
-import { recorderReducer as recorder } from 'react-recorder-redux'
 
 import entitiesReducer, { stubbedEntityReducers } from './entitiesReducer'
 
@@ -17,7 +16,6 @@ const rootReducer = combineReducers({
   auth,
   drawing,
   form: formReducer,
-  recorder,
   router: routerReducer,
   stream,
   streamDataByStream,
