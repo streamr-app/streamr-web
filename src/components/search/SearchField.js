9aeab40030783696f211bb717d83bcce41840934
Steven Petryk <me@stevenpetryk.com>
2017-04-15 11:32:55 -0400

    Implement search (#44)
    * Implement search
    
    * Hide search field in recording view
    
    * Enable forward slash search shortcut
    
    * Avoid abbrevs

---
+++ src/components/search/SearchField.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Field } from 'redux-form'
+import Mousetrap from '../Mousetrap'
+
+import {
+  TextField,
+  reduxFormWrapper
+} from '../fields'
+
+const searchField = reduxFormWrapper(<TextField id='email' />)
+
+export default class SearchField extends React.Component {
+  focusSearch (event) {
+    event.preventDefault()
+
+    const node = ReactDOM.findDOMNode(this.search)
+    node.querySelector('input').focus()
+  }
+
+  render () {
+    const {
+      handleSubmit,
+      onSubmit
+    } = this.props
+
+    return (
+      <div className='search'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Field name='search' ref={(search) => { this.search = search }} placeholder='Search...' component={searchField} />
+          <input type='submit' style={{ display: 'none' }} />
+        </form>
+
+        <Mousetrap
+          bindings={{
+            '/': (event) => this.focusSearch(event)
+          }}
+        />
+      </div>
+    )
+  }
+}
