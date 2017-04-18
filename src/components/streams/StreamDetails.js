31bd48df37d312b9c612901d51e9c85b460afef5
Tyler <tnpetresky@gmail.com>
2017-04-17 22:06:07 -0400

    Add Topic Display to Stream View (#69)
    * add the topic display to the stream card
    
    * styling for topic info
    
    * hide topic if not there
    
    * error
    
    * hiding the topic container

--- src/components/streams/StreamDetails.js
+++ src/components/streams/StreamDetails.js
@@ -4,11 +4,12 @@ import UserCardContainer from '../users/UserCardContainer'
 import VoteButtonContainer from './VoteButtonContainer'
 
 export default ({
-  stream = {}
+  stream = {},
+  topic
 }) => (
   <div className='stream-details'>
     <div className='basic-info'>
-      <h1>{stream.title}</h1>
+      <h1>{stream.title}</h1> {(topic) ? <h4>{topic.name}</h4> : null}
       <p>{stream.description || noDescription()}</p>
 
       {stream.id &&
