4850543c95a3d524bf3f2d9a6e9bd1b44e68d8a7
Steven Petryk <me@stevenpetryk.com>
2017-04-18 01:34:59 -0400

    Add undo keyboard shortcut (#75)
    * Add cmd+z undo shortcut
    
    * Support ctrl-z

--- src/components/draw/UndoButton.js
+++ src/components/draw/UndoButton.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import cx from 'classnames'
 
+import Mousetrap from '../Mousetrap'
+
 export default ({
   onClick,
   disabled
 }) => (
-  <div className={cx('sidebar-button undo-button', { disabled })} {...{ onClick }} />
+  <div className={cx('sidebar-button undo-button', { disabled })} {...{ onClick }}>
+    <Mousetrap
+      bindings={{
+        'command+z': (e) => { onClick(); e.preventDefault() },
+        'ctrl+z': (e) => { onClick(); e.preventDefault() }
+      }}
+    />
+  </div>
 )
