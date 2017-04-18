2fbe1e0aecee490e04cf897a068b5f9d60e819ac
Steven Petryk <me@stevenpetryk.com>
2017-04-18 01:09:25 -0400

    Add cmd+z undo shortcut

--- src/components/draw/UndoButton.js
+++ src/components/draw/UndoButton.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import cx from 'classnames'
 
+import Mousetrap from '../Mousetrap'
+
 export default ({
   onClick,
   disabled
 }) => (
-  <div className={cx('sidebar-button undo-button', { disabled })} {...{ onClick }} />
+  <div className={cx('sidebar-button undo-button', { disabled })} {...{ onClick }}>
+    <Mousetrap
+      bindings={{
+        'command+z': () => onClick()
+      }}
+    />
+  </div>
 )
