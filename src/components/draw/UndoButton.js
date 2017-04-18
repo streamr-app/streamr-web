65e87c769109aa979a770bd92eb60ca5a8691804
Steven Petryk <me@stevenpetryk.com>
2017-04-18 01:10:40 -0400

    Support ctrl-z

--- src/components/draw/UndoButton.js
+++ src/components/draw/UndoButton.js
@@ -10,7 +10,8 @@ export default ({
   <div className={cx('sidebar-button undo-button', { disabled })} {...{ onClick }}>
     <Mousetrap
       bindings={{
-        'command+z': () => onClick()
+        'command+z': (e) => { onClick(); e.preventDefault() },
+        'ctrl+z': (e) => { onClick(); e.preventDefault() }
       }}
     />
   </div>
