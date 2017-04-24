2da5180251017904d26b9d3b04b73d5e60f439c8
Steven Petryk <me@stevenpetryk.com>
2017-04-24 17:41:30 -0400

    Remove react-storybook

--- src/components/stories/buttons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
-
-import { Button } from '../buttons'
-
-storiesOf('Buttons', module)
-  .add('Standard button', () => (
-    <Button onClick={action('Clicked')}>I'm a button!</Button>
-  ))
--- src/components/stories/data/channel.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: 'Tyler Petresky',
-  channelName: 'h3h3Productions',
-  bio: 'Goofs, gaffs, spoofs, and riots. Fresh memes!',
-  subscriberCount: 420,
-  profileImage: 'http://placehold.it/60x60'
-}
