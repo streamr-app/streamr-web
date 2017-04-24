8bc977013670a31fbea93bbaffcd892402fa5488
Steven Petryk <me@stevenpetryk.com>
2017-04-24 16:59:29 -0400

    Eliminate RecordLayout

--- src/components/recording/DrawView.js
+++ src/components/recording/DrawView.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import cx from 'classnames'
 
+import Helmet from 'react-helmet'
 import { Prompt } from 'react-router-dom'
 
-import MouseDrawingHandlerContainer from './MouseDrawingHandlerContainer'
-import StreamRendererContainer from './StreamRendererContainer'
 import DrawingSidebar from './DrawingSidebar'
+import StreamRendererContainer from './StreamRendererContainer'
+import MouseDrawingHandlerContainer from './MouseDrawingHandlerContainer'
 import RecordingErrorsContainer from './RecordingErrorsContainer'
 
 export default ({
   recording
 }) => (
   <div className={cx('draw-view', { disabled: !recording })}>
+    <Helmet htmlAttributes={{ class: 'recording' }} />
+
     <Prompt
       when={recording}
       message='Are you sure you want to leave? Your recording will be cancelled.' />
@@ -21,7 +24,6 @@ export default ({
     <div className='draw-container'>
       <StreamRendererContainer />
       <MouseDrawingHandlerContainer />
-
       <RecordingErrorsContainer />
     </div>
   </div>
