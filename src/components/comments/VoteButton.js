201cc894471f788936ec3ff606dfe6d1be078aa9
Greg White <gkwhite95@gmail.com>
2017-05-30 07:57:32 -0400

    Fix VoteButton click handler

--- src/components/comments/VoteButton.js
+++ src/components/comments/VoteButton.js
@@ -11,7 +11,7 @@ export default ({
   return (
     <div
       className={cx('vote-button', { active: userHasVoted, disabled: !isSignedIn })}
-      onClick={(e) => isSignedIn && (userHasVoted ? onUnvote() : onUpvote())}
+      onClick={() => (isSignedIn && userHasVoted) ? onUnvote() : onUpvote()}
     >
       <i className='fa fa-thumbs-up' />
       <span>{numVotes}</span>
