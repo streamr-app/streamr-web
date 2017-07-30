d0eb8b8a3f7f96e2e0928686f65beadb39788050
Steven Petryk <me@stevenpetryk.com>
2017-07-30 15:22:18 -0400

    Use immutability helper to refactor reducers

--- src/reducers/comment.js
+++ src/reducers/comment.js
@@ -1,24 +1,26 @@
+import update from 'immutability-helper'
+
 export default function (state = {}, action) {
   switch (action.type) {
     case 'COMMENT_UPVOTE_SUCCESS':
-      return {
-        ...state,
+      return update(state, {
         [action.payload.commentId]: {
-          ...state[action.payload.commentId],
-          currentUserVoted: true,
-          votesCount: state[action.payload.commentId].votesCount + 1
+          $merge: {
+            currentUserVoted: true,
+            votesCount: state[action.payload.commentId].votesCount + 1
+          }
         }
-      }
+      })
 
     case 'COMMENT_UNVOTE_SUCCESS':
-      return {
-        ...state,
+      return update(state, {
         [action.payload.commentId]: {
-          ...state[action.payload.commentId],
-          currentUserVoted: false,
-          votesCount: state[action.payload.commentId].votesCount - 1
+          $merge: {
+            currentUserVoted: false,
+            votesCount: state[action.payload.commentId].votesCount - 1
+          }
         }
-      }
+      })
 
     default:
       return state
--- src/reducers/streamDataByStream.js
+++ src/reducers/streamDataByStream.js
@@ -1,9 +1,11 @@
+import update from 'immutability-helper'
+
 export default function (state = {}, action) {
   switch (action.type) {
     case 'STREAM_DATA_SUCCESS': {
       const streamId = action.payload.streamId
 
-      return { ...state, [streamId]: action.payload.data }
+      return update(state, { [streamId]: { $set: action.payload.data } })
     }
   }
 
